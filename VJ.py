4c51377b0456b11daedf173dd2e5953acf32b996
Dedewahyu10 <62569041+Dedewahyu10@users.noreply.github.com>
2020-03-24 14:56:14 +0700

    Add files via upload

---
+++ VJ.py
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Hacked by (vjwahyu)</title>
+<iframe>https://github.com/Dedewahyu10/dark-fb/<iframe>
+</head>
+<body bgcolor="black" >
+<center><font face="Courier new" size="24" color="lime"> VJWAHYU HACK </font>
+<br><img src="https://https://www.google.com/search?q=gambar+hacker&safe=strict&client=ms-android-xiaomi&prmd=isvn&sxsrf=ALeKk01XIlDN_5qNWERTd5Kvv3yJjWPLAA:1585035188122&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiFueCazLLoAhUEeysKHbRmC4YQ_AUoAXoECAoQAQ&biw=450&bih=720#imgrc=XZbUdx9OR1_Q8M/root.png"width="300"height="300">
+<br><h1><span style="color:#ffffff;font-family:Iceland;text-shadow:#FF0099 0px 0px 10px">[+] Hacked by VJWAHYU [+]</span></h1>
+<font face="Courier new" size="6" color="red"> Thanks to : </font>
+<br>
+<marquee behavior="scroll" direction="left" scrollamount="4" scrolldelay="55" width="100%">
+<font face="Courier New" size="5" font style="text-shadow: 0px 0px 20px blue;" color="white">
+<b>-=| menyentuh tanpa menyentuh, JANGAN GANGGU,JANGAN NGUSIK |=-</font>
+</body>
+</html>
